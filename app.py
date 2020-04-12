a185d9d73999797a00cb920a07e5b70ab86232bf
Adriano Medeiros dos Santos <adrianomedeirossantos@gmail.com>
2020-04-12 18:41:22 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+import logging
+import os
+import redis
+import ssl as ssl_lib
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import certifi
+from flask import Flask
+from slack import WebClient
+from slackeventsapi import SlackEventAdapter
+
+from stand_up import StandUp, StandUpService
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+app = Flask(__name__)
+slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app)
+slack_web_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+channel = "G0129VCNU5N"
+stand_up_service = StandUpService(slack_client=slack_web_client, redis=r)
+
+@app.route('/stand_up')
+def trigger_stand_up():
+    response = slack_web_client.conversations_members(channel=channel)
+    members = response.data['members']
+    for m in members:
+        stand_up = StandUp(user=m, channel=channel)
+        stand_up_service.start(stand_up)
+
+    return 'Stand up just started!'
+
+
+@slack_events_adapter.on("message")
+def message(payload):
+    event = payload.get("event", {})
+
+    if event["channel_type"] != "im":
+        return
+
+    user = event.get("user")
+    if 'bot_profile' in event:
+        return
+
+    stand_up_service.on_update(user, event["text"])
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+    ssl_context = ssl_lib.create_default_context(cafile=certifi.where())
+
+    scheduler = BackgroundScheduler()
+    start_date = datetime(2020, 4, 12, 9, 50)
+    scheduler.add_job(func=trigger_stand_up, trigger=IntervalTrigger(days=1, start_date=start_date))
+    scheduler.start()
+    app.run(port=9292)
---
+++ stand_up.py
@@ -0,0 +1,123 @@
+import csv
+import random
+import pickle
+import redis
+from slack import WebClient
+
+
+class StandUp:
+    def __init__(self, user, channel):
+        self.user = user
+        self.channel = channel
+        self.questions = [
+            "What did you do yesterday?",
+            "What are you going to do today?",
+            "Any blockers?"
+        ]
+        self.number_of_questions = len(self.questions)
+        self.answered_questions = []
+        self.answers = []
+
+    def has_started(self):
+        return len(self.questions) == self.number_of_questions
+
+    def has_pending_questions(self):
+        return len(self.questions) > 0
+
+    def next_question(self):
+        next = self.questions.pop(0)
+        self.answered_questions.append(next)
+        return next
+
+
+class StandUpService:
+    def __init__(self, slack_client: WebClient, redis: redis.Redis):
+        self.slack_client = slack_client
+        self.redis = redis
+        self.greetings = self._load_messages('greetings.csv')
+        self.good_bye = self._load_messages('good_bye.csv')
+
+    def start(self, stand_up):
+        greetings = self._greetings()
+
+        self.slack_client.chat_postMessage(
+            channel=stand_up.user,
+            text=f'{greetings}\n{stand_up.next_question()}',
+        )
+
+        pickled_object = pickle.dumps(stand_up)
+        self.redis.set(stand_up.user, pickled_object)
+
+        return
+
+    def on_update(self, user, answer):
+        packed_stand_up = self.redis.get(user)
+        if packed_stand_up is None:
+            return
+
+        stand_up = pickle.loads(packed_stand_up)
+
+        stand_up.answers.append(answer)
+
+        if stand_up.has_pending_questions():
+            self.slack_client.chat_postMessage(
+                channel=stand_up.user,
+                text=stand_up.next_question(),
+            )
+            pickled_object = pickle.dumps(stand_up)
+            self.redis.set(stand_up.user, pickled_object)
+        else:
+            self.slack_client.chat_postMessage(
+                channel=stand_up.user,
+                text=self._good_bye(),
+            )
+
+            self._send_summary(stand_up)
+            self.redis.delete(stand_up.user)
+
+    def _greetings(self):
+        message = self.greetings[random.randint(0, len(self.greetings)-1)]
+        return f'{message}\nIt\'s time for our *Standup*!'
+
+    def _send_summary(self, stand_up):
+        q = list(map(lambda x: f'*{x}*', stand_up.answered_questions))
+        a = list(map(lambda x: f'{x}', stand_up.answers))
+
+        user_info = self.slack_client.users_info(user=stand_up.user)
+        result = [val for pair in zip(q, a) for val in pair]
+        result = "\n".join(result)
+        self.slack_client.chat_postMessage(
+            channel=stand_up.channel,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f'*{user_info["user"]["name"]}*\'s update',
+                    }
+                },
+                {
+                    "type": "context",
+                    "elements": [
+                        {
+                            "type": "mrkdwn",
+                            "text": result
+                        }
+                    ]
+                },
+            ]
+        )
+
+    def _good_bye(self):
+        message = self.good_bye[random.randint(0, len(self.good_bye)-1)]
+        return f'{message}\n I have to go to the beach now :beach_with_umbrella:!'
+
+    def _load_messages(self, filename):
+        rows = []
+
+        with open(filename) as csvDataFile:
+            csvReader = csv.reader(csvDataFile, delimiter="\n")
+            for row in csvReader:
+                rows.append(row[0])
+
+        return rows
