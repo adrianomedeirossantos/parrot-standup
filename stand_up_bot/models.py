2519d1ebea2c72c3ccf5210c8cb42046d845e1aa
Adriano Medeiros dos Santos <adrianomedeirossantos@gmail.com>
2020-07-31 16:25:51 +0100

    Adds stand_up_template model

--- stand_up_bot/models.py
+++ stand_up_bot/models.py
@@ -1,23 +1,25 @@
-class StandUp:
-    def __init__(self, user, channel):
-        self.user = user
+class StandUpTemplate:
+    def __init__(self, channel, cron_expression, questions):
         self.channel = channel
-        self.questions = [
-            "What did you do yesterday?",
-            "What are you going to do today?",
-            "Any blockers?",
-        ]
-        self.number_of_questions = len(self.questions)
+        self.cron_expression = cron_expression
+        self.questions = questions
+
+
+class StandUp:
+    def __init__(self, user, template):
+        self.user = user
+        self.template = template
+        self.number_of_questions = len(self.template.questions)
         self.answered_questions = []
         self.answers = {}
 
     def has_started(self):
-        return len(self.questions) == self.number_of_questions
+        return len(self.template.questions) == self.number_of_questions
 
     def has_pending_questions(self):
-        return len(self.questions) > 0
+        return len(self.template.questions) > 0
 
     def next_question(self):
-        next = self.questions.pop(0)
+        next = self.template.questions.pop(0)
         self.answered_questions.append(next)
         return next
