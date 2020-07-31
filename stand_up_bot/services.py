0beeddc1173cb1664d457faabb47c7202793f2f1
Adriano Medeiros dos Santos <adrianomedeirossantos@gmail.com>
2020-07-31 16:27:54 +0100

    Implements stand_up_scheduler

--- stand_up_bot/services.py
+++ stand_up_bot/services.py
@@ -1,9 +1,12 @@
 import abc
 import csv
+import os
 import pickle
 import random
 import redis
 
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 from slack import WebClient
 
 from stand_up_bot.models import StandUp, StandUpTemplate
@@ -85,85 +88,21 @@ class StandUpTemplateEnvRepository(StandUpTemplateRepository):
 class StandUpService:
     def __init__(
             self,
-            slack_client: WebClient,
-            redis: redis.Redis,
-            salutation_service: SalutationService
+            stand_up_service: StandUpService,
+            stand_up_template_repository: StandUpTemplateRepository
     ):
-        self.slack_client = slack_client
-        self.redis = redis
-        self.salutation_service = salutation_service
+        self.stand_up_service = stand_up_service
+        self.stand_up_template_repository = stand_up_template_repository
+        self.scheduler = BackgroundScheduler()
 
-    def start(self, stand_up):
-        intro = "It's time for our *Standup*!"
-        greetings = f"{self.salutation_service.say_hi()}\n{intro}"
-
-        start_msg = f"{greetings}\n{stand_up.next_question()}"
-        self.slack_client.chat_postMessage(
-            channel=stand_up.user, text=start_msg,
-        )
-
-        pickled_object = pickle.dumps(stand_up)
-        self.redis.set(stand_up.user, pickled_object)
-
-        return
-
-    def update_answer(self, user, message_id, answer):
-        packed_stand_up = self.redis.get(user)
-
-        if packed_stand_up is None:
-            return
-
-        stand_up = pickle.loads(packed_stand_up)
-        stand_up.answers[message_id] = answer
-        pickled_object = pickle.dumps(stand_up)
-        self.redis.set(user, pickled_object)
-
-    def store_answer(self, user, message_id, answer):
-        packed_stand_up = self.redis.get(user)
-        if packed_stand_up is None:
-            return
-
-        stand_up = pickle.loads(packed_stand_up)
-
-        stand_up.answers[message_id] = answer
-
-        if stand_up.has_pending_questions():
-            self.slack_client.chat_postMessage(
-                channel=stand_up.user, text=stand_up.next_question(),
-            )
-            pickled_object = pickle.dumps(stand_up)
-            self.redis.set(stand_up.user, pickled_object)
-        else:
-            self.slack_client.chat_postMessage(
-                channel=stand_up.user,
-                text=self.salutation_service.say_good_bye(),
+    def start(self):
+        templates = self.stand_up_template_repository.find_all()
+        for t in templates:
+            cron_trigger = CronTrigger.from_crontab(t.cron_expression)
+            self.scheduler.add_job(
+                func=self.stand_up_service.trigger,
+                trigger=cron_trigger,
+                args=[t]
             )
 
-            self._send_summary(stand_up)
-            self.redis.delete(stand_up.user)
-
-    def _send_summary(self, stand_up):
-        q = list(map(lambda x: f"*{x}*", stand_up.answered_questions))
-        a = list(map(lambda x: f"{x}", stand_up.answers.values()))
-
-        user_info = self.slack_client.users_info(user=stand_up.user)
-        result = [val for pair in zip(q, a) for val in pair]
-        result = "\n".join(result)
-        self.slack_client.chat_postMessage(
-            channel=stand_up.channel,
-            blocks=[
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f'*{user_info["user"]["name"]}*\'s update',
-                    },
-                },
-                {
-                    "type": "context",
-                    "elements": [
-                        {"type": "mrkdwn", "text": result}
-                    ]
-                },
-            ],
-        )
+        self.scheduler.start()
